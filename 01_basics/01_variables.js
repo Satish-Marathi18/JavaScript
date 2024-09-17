34998aad6620f7936b1b496175d2acb5b3eaffd8
Satish-Marathi18 <marathisatish17@gmail.com>
2024-09-17 07:05:41 +0000

    variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,14 @@
+const accountId = 4340
+let accountEmail = "satish123@gamil.com"
+var accountPassword = "12345"
+accountCity = "Jaipur"
+
+// accountId = 4341  (Not allowed)
+
+console.log(accountId);
+
+accountEmail = "satish@gmail.com"
+accountPassword = "Satish@123"
+accountCity = "Bengalore"
+
+console.table([accountId,accountEmail,accountPassword,accountCity])
