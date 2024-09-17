d080c76c47d834f73e473aceec9b987443eb76a1
Satish-Marathi18 <marathisatish17@gmail.com>
2024-09-17 07:04:55 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,28 @@
+"use strict"  //treat all JS code as newer version
+
+//alert(3 + 3) //we are using nodejs, not browser
+
+console.log(3
+    +
+    3) //code redability should be high
+
+    let name = "Satish"
+    let age = 22
+    let isLoggedIn = false
+
+    //number => 2 to the power of 53
+    //bigint
+    //string => ""
+    //boolean => true/false
+    //null => standalone value
+    //undefined =>
+    //symbol => unique
+
+    //object
+
+    console.log(typeof "Satish") //string
+    console.log(typeof age) //number
+    console.log(typeof null) //object
+    console.log(typeof undefined) //undefined
+    
+    
