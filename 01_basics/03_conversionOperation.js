e4594736e179cf1724b1c5cc66e4f4b2255e53ff
Satish-Marathi18 <marathisatish17@gmail.com>
2024-09-17 07:42:58 +0000

    Conversion of datatype

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,19 @@
+let score = true
+
+// console.log(typeof score); //boolean
+// console.log(typeof(score)); //boolean
+
+// let valueInNumber = Number(score); //type conversion
+// console.log(valueInNumber); //NaN (Not a Number)
+// console.log(typeof(valueInNumber)); //number
+
+let isLoggedIn = "Satish"
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn); //true
+
+//1=> true ; 0=> false
+//"" => fasle; "Satish"=> true
+
+let someNumber = 33
+let stringNumber = String(someNumber)
+console.log(typeof stringNumber) //string
